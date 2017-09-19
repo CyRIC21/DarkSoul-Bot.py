2514b23df34cb12fcb70cdc2d27bcadc16343144
freetnt5852 <31079629+freetnt5852@users.noreply.github.com>
2017-09-19 14:02:48 -0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ import os
 from discord.ext import commands
 bot = commands.Bot(command_prefix='?',description="DarkSoul-Bot\nOwner: Free TNT#5796\n\nHelp Commands",owner_id=292690616285134850)
 bot.load_extension("cogs.utility")
+bot.load_extension("cogs.misc")
 
 @bot.event
 async def on_ready():
