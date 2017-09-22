a50187b828dfc3dcb20307cac623ea67bd44dafd
freetnt5852 <31079629+freetnt5852@users.noreply.github.com>
2017-09-22 08:34:37 -0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -43,23 +43,7 @@ async def ping(ctx):
 async def invite(ctx):
     await ctx.send('https://discordapp.com/oauth2/authorize?client_id=358300617665544194&scope=bot&permissions=66186303')
     
-def cleanup_code(content):
-    """Automatically removes code blocks from the code."""
-    # remove ```py\n```
-    if content.startswith('```') and content.endswith('```'):
-        return '\n'.join(content.split('\n')[1:-1])
 
-    # remove `foo`
-    return content.strip('` \n')
-
-def get_syntax_error(e):
-    if e.text is None:
-        return f'```py\n{e.__class__.__name__}: {e}\n```'
-    return f'```py\n{e.text}{"^":>{e.offset}}\n{e.__class__.__name__}: {e}```'
-  
-async def(ctx, body):
-    msg = f'```py\n{body}\n```'
-    await ctx.message.edit(content=msg)
 
 @bot.command(name='eval')
 @commands.is_owner()
