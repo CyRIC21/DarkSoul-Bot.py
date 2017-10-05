b8d287fd53239b526efd970028016bd40caf45ba
umbresp <30674647+umbresp@users.noreply.github.com>
2017-10-04 21:47:12 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,16 +1,31 @@
-import discord
-import os
-import traceback
-import textwrap
-import io
-from discord.ext import commands
-from contextlib import redirect_stdout
+# _________                                          /\        __________        __   
+# \_   ___ \_______   ____   ____ ______   __________)/ ______ \______   \ _____/  |_ 
+# /    \  \/\_  __ \_/ __ \_/ __ \\____ \_/ __ \_  __ \/  ___/  |    |  _//  _ \   __\
+# \     \____|  | \/\  ___/\  ___/|  |_> >  ___/|  | \/\___ \   |    |   (  <_> )  |  
+#  \______  /|__|    \___  >\___  >   __/ \___  >__|  /____  >  |______  /\____/|__|  
+#         \/             \/     \/|__|        \/           \/          \/             
+
+# (some made by umbresp)
+
+try:
+    import discord
+    import os
+    import traceback
+    import textwrap
+    import io
+    from discord.ext import commands
+    from contextlib import redirect_stdout
+except:
+    print("You do not have one of the required dependencies.")
+    exit(0) # Exit code 0 = error
+
+# The 4 lines below are a mess, will rewrite
 bot = commands.Bot(command_prefix='?',description="Creeper's Bot\nOwner: CreeperSlayer11#7929\n\nHelp Commands",owner_id=311970805342928896)
 bot.load_extension("cogs.utility")
 bot.load_extension("cogs.misc")
 bot.load_extension("cogs.mod")
 
-
+# useless method but idk why it's here
 def cleanup_code(content):
     """Automatically removes code blocks from the code."""
     # remove ```py\n```
@@ -19,30 +34,38 @@ def cleanup_code(content):
 
     return content.strip('` \n')
 
+# Print to console once bot is online
 @bot.event
 async def on_ready():
-  print('Bot is online!')
+  print("Login successful.") # Add ascii art?
   
 @bot.command(name='say')
 async def say(ctx, message:str):
     """Say Something As The Bot"""
-    await ctx.send(message)
- 
-
-@bot.command(name='presence')
+    if not message: # if the message is blank
+        return
+    else:
+        await ctx.send(message)
+        
+# Since you seem to have copied the next cmd, I'll rewrite it besides this is horrible anyways lol
+# also I renamed it to setpresence
+# oh the horror of single quotes
+        
+@bot.command(name="setpresence")
 @commands.is_owner()
-async def _set(ctx, Type=None,*,thing=None):
+async def presence(ctx, Type=None, *, msg=None):
   """Change the bot's discord game/stream!"""
-  if Type is None:
-    await ctx.send('Usage: `.presence [game/stream] [message]`')
+  if Type is None and thing is None: # latter should never happen without first also happening but we want to be safe
+    await ctx.send('You need to specify something to play/stream.')
   else:
-    if Type.lower() == 'stream':
-      await bot.change_presence(game=discord.Game(name=thing,type=1,url='https://www.twitch.tv/a'),status='online')
+    Type.lower() = Type # saves us the extra work
+    if Type == 'stream':
+      await bot.change_presence(game=discord.Game(name=msg,type=1,url="https://www.twitch.tv/iwouldputmytwitchchannelherebutidonthaveone"),status='online')
       await ctx.send(f'Set presence to. `Streaming {thing}`')
-    elif Type.lower() == 'game':
+    elif Type == 'game':
       await bot.change_presence(game=discord.Game(name=thing))
       await ctx.send(f'Set presence to `Playing {thing}`')
-    elif Type.lower() == 'clear':
+    elif Type == 'clear':
       await bot.change_presence(game=None)
       await ctx.send('Cleared Presence')
     else:
@@ -50,16 +73,15 @@ async def _set(ctx, Type=None,*,thing=None):
   
 @bot.command()
 async def ping(ctx):
-    """Pong! Returns your websocket latency."""
-    em = discord.Embed()
-    em.title ='Pong! Websocket Latency:'
-    em.description = f"{bot.ws.latency * 1000:.4f} ms"
-    await ctx.send(embed=em)
+    """return websocket latency."""
+    await ctx.send("Websocket Latency:")
+    await ctx.send(f"{bot.ws.latency * 1000:.4f} ms")
     
 
 @bot.command()
 async def invite(ctx):
-    await ctx.send('https://discordapp.com/oauth2/authorize?client_id=364372021422981120&scope=bot&permissions=66186303')
+    await ctx.send("**Invite me to your server!**"
+    await ctx.send("https://discordapp.com/oauth2/authorize?client_id=364372021422981120&scope=bot&permissions=66186303")
     
 
 
@@ -110,8 +132,18 @@ async def _eval(ctx, *, body: str):
                 await ctx.send(f'```py\n{value}{ret}\n```')
 
 
-
-
+# run as "main" method
+if __name__ == "__main__":
+    print("_________                                          /\        __________        __   ")
+    print("\_   ___ \_______   ____   ____ ______   __________)/ ______ \______   \ _____/  |_ ")
+    print("/    \  \/\_  __ \_/ __ \_/ __ \\____ \_/ __ \_  __ \/  ___/  |    |  _//  _ \   __\")
+    print("\     \____|  | \/\  ___/\  ___/|  |_> >  ___/|  | \/\___ \   |    |   (  <_> )  |  ")
+    print(" \______  /|__|    \___  >\___  >   __/ \___  >__|  /____  >  |______  /\____/|__|  ")
+    print("        \/             \/     \/|__|        \/           \/          \/             ")
+    # laziness
+          
+    print("Logging in to Discord...")
+                
 if not os.environ.get('TOKEN'):
   print("no token found REEEE!")
 bot.run(os.environ.get('TOKEN').strip('\"'))
